c8400b0672303a25fb2e8c870897f94e4c1edf8b
Philipp Fromme <philippfromme@outlook.com>
2018-11-29 14:24:57 +0100

    feat(menu): add menu entries

--- menu.js
+++ menu.js
@@ -10,9 +10,29 @@ module.exports = function(electronApp, menuState) {
       return menuState.bpmn && menuState.inactiveInput;
     },
     action: function() {
-
-      // will be forwarded to bpmn-js eventually
       electronApp.emit('menu:action', 'toggleTokenSimulation');
+    }
+  }, {
+    label: 'Play/Pause Simulation',
+    accelerator: 'Space',
+    enabled: function() {
+
+      // only enabled for BPMN diagrams
+      return menuState.bpmn && menuState.inactiveInput;
+    },
+    action: function() {
+      electronApp.emit('menu:action', 'togglePauseTokenSimulation');
+    }
+  }, {
+    label: 'Reset Simulation',
+    accelerator: 'r',
+    enabled: function() {
+
+      // only enabled for BPMN diagrams
+      return menuState.bpmn && menuState.inactiveInput;
+    },
+    action: function() {
+      electronApp.emit('menu:action', 'resetTokenSimulation');
     }
   }];
 };
