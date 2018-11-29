5467d74036e4b5c0f192739e7fb7f5c7e919a2be
Philipp Fromme <philippfromme@outlook.com>
2018-11-29 14:35:21 +0100

    chore(project): migrate to webpack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   name: 'bpmn-js Token Simulation',
-  script: './client/client-bundle.js',
+  script: './client/client.bundle.js',
   style: './css/bpmn-js-token-simulation.css',
   menu: './menu.js'
 };
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './client/client.js',
+  output: {
+    path: path.resolve(__dirname, 'client'),
+    filename: 'client.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
