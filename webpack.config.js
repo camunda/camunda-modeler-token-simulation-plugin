aaa32c373b2aea9b83522f678ed65e27f6e53251
Nico Rehwaldt <git_nikku@nixis.de>
2018-12-04 23:59:28 +0100

    chore(build): do not minify
    * keep things unminified so errors can be debugged
      more easily

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './client/client.js',
   output: {
     path: path.resolve(__dirname, 'client'),
