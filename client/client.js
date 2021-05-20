e999b955ef8f8c34e89916a1361178a4abda0cc1
Nico Rehwaldt <git_nikku@nixis.de>
2021-05-20 10:40:52 +0200

    deps: bump to bpmn-js-token-simulation@0.19.3
    + add bpmn-js as a required peer dependency
    
    Closes #29
    Closes #30

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 var registerBpmnJSPlugin = require('camunda-modeler-plugin-helpers').registerBpmnJSPlugin;
 
-var tokenSimulation = require('bpmn-js-token-simulation'),
+var tokenSimulation = require('bpmn-js-token-simulation').default,
     HideModelerElements = require('./HideModelerElements');
 
 tokenSimulation.__init__.push('hideModelerElements');
